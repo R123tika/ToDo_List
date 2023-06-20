b9b9eee3f4e87d015e431644dcda2e33f71126e2
Ritika <Ritikaverma3147@gmail.com>
2023-06-20 12:07:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import List from "./List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function App(){
+  const state =useState();
+  const[prev,curr]=useState();
+  const[prevclick,currclick]=useState([]);
+  function write(event){
+    curr(event.target.value)
+  }
+  function click(){
+    currclick((pre)=>{
+      return([...pre,prev])
+      
+    }) 
+    curr('')
+  }
+  function remove(id){
+    currclick((olditem)=>{
+      return(olditem.filter((item,index)=>{
+        return index !== id;
+      }))
+    })
+  }
+
+  return(
+    <>
+      <div className="main">
+        <div className="center">
+        <h1>ToDo List</h1>
+          <input type='text' placeholder ='Add a Items'
+            value={prev}
+            onChange={write}
+          />
+          <span>
+            <button onClick={click}>+</button>
+          </span>
+          <ol>
+            {
+              prevclick.map((arr,index)=>{
+               return( 
+                <List
+                  value ={arr}
+                  key={index}
+                  id={index}
+                  change ={remove}
+
+               />)
+              })
+            }
+          </ol>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
